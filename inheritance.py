aeb72b17a932a911e54382ed7906df47e137af32
Sadiq Okocha <sokocha@gmail.com>
2017-09-20 16:03:20 +0000

    creates git repository

---
+++ inheritance.py
@@ -0,0 +1,74 @@
+with open("eits.csv",'r') as f:
+  for line in f.readlines():
+    print (line)
+
+class School():
+  def __init__(self, eits=[],fellows=[]):
+    self.eits = eits
+    self.fellows = fellows
+
+  def register(self,eit):
+    self.eits.append(eit)
+    return print ("eit {} saved successfully".format(eit.name))
+
+  def hire(self,fellow):
+    self.fellows.append(fellow)
+    return print ("fellow {} saved successfully".format(fellow.name))
+
+  def fire(self,fellow):
+    self.fellows.remove(fellow)
+    return print ("Prez Trump says 'you're FIRED {}".format(fellow.name))
+
+
+class EIT():
+  def __init__(self, name,nationality="Earthling"):
+    self.name = name
+    self.nationality = nationality
+
+
+
+
+  # def drop_knowledge(self, facts=[]):
+  #   self.facts = facts
+
+
+
+
+
+class Fellow():
+  def __init__(self,name,nationality,happiness_level=5):
+    self.name = name
+    self.nationality = nationality
+    self.happiness_level = happiness_level
+
+  def eat(self):
+    self.happiness_level += 10
+    print("Yummy, I love banku. happiness level now {}".format(self.happiness_level))
+
+  def teach(self):
+    self.happiness_level -= 7
+    print ("I hate teaching. Happiness level now {}".format(self.happiness_level))
+
+
+
+
+mest = School()
+sadiq = EIT(name = "sadiq")
+miishe = Fellow("Miishe","ghanarican")
+francis = Fellow("Francis","ghanaian")
+andrew = Fellow("drew","amurricuh")
+
+# print(sadiq.name,sadiq.nationality)
+print ("hi i'm {}, my nationality is {} and my current happiness is {}".format(miishe.name, miishe.nationality, miishe.happiness_level))
+
+miishe.eat()
+# miishe.teach()
+
+mest.hire(miishe)
+mest.hire(francis)
+mest.hire(andrew)
+mest.register(sadiq)
+mest.fire(andrew)
+
+
+
